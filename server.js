03fa6cee9cc15ee4467e6ff8a794baecb2be9fdf
Juanjuan <juan55wang@gmail.com>
2023-12-02 10:00:30 +1100

    create a express node server, handle router,create logger file to log requst and error...

--- server.js
+++ server.js
@@ -1,107 +1,47 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-
-const logEvents = require('./logEvents');
-const EventEmitter = require('events');
-class Emitter extends EventEmitter{};
-
-// initialize object;
-const myEmitter = new Emitter();
-myEmitter.on('log', (msg, fileName)=>logEvents(msg, fileName));
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const {logger} = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
-const serveFile = async (filePath, contentType, response) => {
-    try {
-        const rawData = await fsPromises.readFile(filePath, 
-            !contentType.includes('image') ? 'utf8' : ''
-            );
-        const data = contentType === 'application/json'
-            ? JSON.parse(rawData): rawData;
-        response.writeHead(
-            filePath.includes('404.html')? 404: 200, 
-            {'content-type': contentType}
-        );
-        response.end(
-            contentType === 'application/json'
-            ? JSON.stringify(data)
-            : data
-        );
-    } catch (err) {
-        console.log(err);
-        myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
-        response.statusCode = 500;
-        response.end();
-    }
-}
+//custom middleware logger
+app.use(logger);
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
-    
-    const extension = path.extname(req.url);
-    console.log(extension);
-    let contentType;
-    switch(extension){
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.jpg':
-            contentType = 'image/jpeg';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.txt':
-            contentType = 'text/plain';
-            break;
-        default:
-            contentType = 'text/html';
-    }
-    let filePath = 
-        contentType === 'text/html' && req.url === '/'
-            ? path.join(__dirname, 'views', 'index.html')
-            : contentType === 'text/html' && req.url.slice(-1) === '/'
-                ? path.join(__dirname, 'views', req.url, 'index.html')
-                : contentType === 'text/html'
-                    ? path.join(__dirname, 'views', req.url)
-                    : path.join(__dirname, req.url)
-    
-    // makes the .html extension not required in the browser
-    if(!extension && req.url.slice(-1) !== '/') filePath += '.html';
+// cross origin resources sharing
+app.use(cors(corsOptions));
 
-    const fileExists = fs.existsSync(filePath);
-    if(fileExists){
-        // serve file
-        serveFile(filePath, contentType, res);
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({extended: false}));
+
+// built-in middleware for json
+app.use(express.json());
+
+//serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
+
+//routes
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
+
+
+app.all('*', (req, res)=>{
+    res.status(404);
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if(req.accepts('json')){
+        res.json({error: '404 not found'});
     } else {
-        //404
-        //301 redirect
-        console.log(path.parse(filePath));
-        switch(path.parse(filePath).base){
-            case 'old-page.html':
-                res.writeHead(301, {'Location': '/new-page.html'});
-                res.end();
-                break;
-            case 'www-page.html':
-                res.writeHead(301, {'Location': '/'});
-                res.end();
-                break;
-            default: 
-            // server 404 response 
-            serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
-        }
+        res.type('txt').send('404 not found');
     }
 });
 
-server.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+app.use(errorHandler);
 
+app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}`));
 
 
